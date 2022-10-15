6d9a13079df3a8c925c6021c782e9505d7d27773
Ribamar FS <ribafs@gmail.com>
2022-10-15 08:29:37 -0300

    login

--- chat/login.php
+++ chat/login.php
@@ -60,6 +60,7 @@ if ( LANG == 'en' ) {
 		<div class="row">
 			<div class="col-sm-6 col-md-4 col-md-offset-4">
 				<h1 class="text-center login-title font"><?=SIGNIN_STR?></h1>
+                <h4 class="text-center"><a href="https://github.com/ribafs/LiveChat">Download deste projeto</a></h4>
 				<div class="account-wall">
 					<img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
 						alt="">
